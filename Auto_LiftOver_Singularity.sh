05da12cbf53605dce28ddc19d4ce20b871acba1a
Friederike David <fdavid@uni-bonn.de>
2026-02-24 12:09:36 +0100

    Use slightly modified version from the FOR2107-adapted ENIGMA SCZ MRS project (introducing a dedicated output directory and removing commented-out code for clarity)

--- Auto_LiftOver_Singularity.sh
+++ Auto_LiftOver_Singularity.sh
@@ -29,6 +29,9 @@ export MEMORY=$((${NCORES} * 7700)) 	## orig = 4 cores with 7700 memory
 ############### END - Do Not Make Changes Beyond This Point ######################
 ##################################################################################
 
+OUT_DIR=${Project_Path}/liftover_tmp
+mkdir -p ${OUT_DIR}
+
 export liftover=${Project_Path}/scripts/liftOver
 
 export plink2="singularity exec --home=$PWD:/home --bind ${Base_Dir} ${Project_Path}/ldpred2.sif plink2"
@@ -36,40 +39,36 @@ export plink1="singularity exec --home=$PWD:/home --bind ${Base_Dir} ${Project_P
 
 # Make Non-Duplicated SNP ID Plink files
 ${plink2} --bfile ${Sample_Dir}/${Prefix} --rm-dup 'force-first' \
-        --make-bed --out ${Sample_Dir}/${Prefix}_noDup --threads ${NCORES} --memory ${MEMORY}
+        --make-bed --out ${OUT_DIR}/${Prefix}_noDup --threads ${NCORES} --memory ${MEMORY}
 
-#pfile_lift="${out_dir}/${batch_name}.lift"
-#if (test $# -eq 1) || (echo "$@" | grep -Fwq lift); then
-#    echo ">>> lift"
-    # Ensure chromosomes are renamed properly for liftover (chr1, ..., chrM etc.).
-    bim_tmp="${Project_Path}/${Prefix}.lift.tmp"
-    ${plink2} --bfile ${Sample_Dir}/${Prefix}_noDup --output-chr chrM \
-        --make-just-bim --out ${bim_tmp} \
-        --threads ${NCORES} --memory ${MEMORY}
-    # If PAR region is split in the batch_bfile, after applying '--output-chr chrM',
-    # SNPs in PAR will be on "chrXY".
-    # Liftover requires SNPs in PAR region to be on chrX, otherwise they will not be lifted corectly.
-    bed_orig="${Project_Path}/${Prefix}.liftover.orig.bed"
-    awk '{if($1=="chrXY") print("chrX",$4-1,$4,$2); else print($1,$4-1,$4,$2);}' \
-        ${bim_tmp}.bim > ${bed_orig}
-    unlifted_snps="${Project_Path}/${Prefix}.unlifted_snps"
-    if [ -f "${liftover_chain}" ]; then
-        echo "Lifting coordinates using ${liftover_chain}"
-        bed_mapped="${Project_Path}/${Prefix}.liftover.mapped.bed"
-        bed_unmapped="${Project_Path}/${Prefix}.liftover.unmapped.bed"
-        ${liftover} ${bed_orig} ${liftover_chain} ${bed_mapped} ${bed_unmapped}
-        grep -Pv "^#" ${bed_unmapped} | cut -f4 > ${unlifted_snps}
-    else
-        echo "liftover_chain is not provided. Coordinates are not lifted."
-        bed_mapped=${bed_orig}
-        touch ${unlifted_snps} # Ensure that (empty) file exists.
-    fi
-    ${plink1} --bfile ${Sample_Dir}/${Prefix}_noDup \
-        --update-chr ${bed_mapped} 1 4 --update-map ${bed_mapped} 3 4 --exclude ${unlifted_snps} \
-        --allow-extra-chr --make-bed --out ${Project_Path}/${Prefix}_b38_lifted \
-        --threads ${NCORES} --memory ${MEMORY}
-    #last_pfile=${pfile_lift}
-    echo "<<< lift"
-#fi
+# Ensure chromosomes are renamed properly for liftover (chr1, ..., chrM etc.).
+bim_tmp="${OUT_DIR}/${Prefix}.lift.tmp"
+${plink2} --bfile ${OUT_DIR}/${Prefix}_noDup --output-chr chrM \
+    --make-just-bim --out ${bim_tmp} \
+    --threads ${NCORES} --memory ${MEMORY}
 
+# If PAR region is split in the batch_bfile, after applying '--output-chr chrM',
+# SNPs in PAR will be on "chrXY".
+# Liftover requires SNPs in PAR region to be on chrX, otherwise they will not be lifted corectly.
+bed_orig="${OUT_DIR}/${Prefix}.liftover.orig.bed"
+awk '{if($1=="chrXY") print("chrX",$4-1,$4,$2); else print($1,$4-1,$4,$2);}' \
+    ${bim_tmp}.bim > ${bed_orig}
+unlifted_snps="${OUT_DIR}/${Prefix}.unlifted_snps"
+
+if [ -f "${liftover_chain}" ]; then
+    echo "Lifting coordinates using ${liftover_chain}"
+    bed_mapped="${OUT_DIR}/${Prefix}.liftover.mapped.bed"
+    bed_unmapped="${OUT_DIR}/${Prefix}.liftover.unmapped.bed"
+    ${liftover} ${bed_orig} ${liftover_chain} ${bed_mapped} ${bed_unmapped}
+    grep -Pv "^#" ${bed_unmapped} | cut -f4 > ${unlifted_snps}
+else
+    echo "liftover_chain is not provided. Coordinates are not lifted."
+    bed_mapped=${bed_orig}
+    touch ${unlifted_snps} # Ensure that (empty) file exists.
+fi
+
+${plink1} --bfile ${OUT_DIR}/${Prefix}_noDup \
+    --update-chr ${bed_mapped} 1 4 --update-map ${bed_mapped} 3 4 --exclude ${unlifted_snps} \
+    --allow-extra-chr --make-bed --out ${OUT_DIR}/${Prefix}_b38_lifted \
+    --threads ${NCORES} --memory ${MEMORY}
 
